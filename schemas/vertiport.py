c99010e4cd7c7f598f28f01bb20042729a82343e
Leandro Bresolin <leandrobresolin@gmail.com>
2025-11-20 09:33:40 -0300

    Updating vertiport schemas

--- schemas/vertiport.py
+++ schemas/vertiport.py
@@ -3,7 +3,7 @@ from typing import Optional
 from uuid import UUID
 
 from ninja import Schema
-from pydantic import ConfigDict
+from pydantic import ConfigDict, RootModel
 
 
 class VertiportSchema(Schema):
@@ -16,3 +16,29 @@ class VertiportSchema(Schema):
     created_at: datetime
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class VertiportSchemaList(RootModel):
+    root: list[VertiportSchema]
+
+
+class SubmitVertiportSchema(Schema):
+    vertiport_code: str
+    vertiport_name: str
+    latitude: Optional[float] = None
+    longitude: Optional[float] = None
+    altitude: Optional[float] = None
+
+
+class UpdateVertiportSchema(Schema):
+    vertiport_code: Optional[str] = None
+    vertiport_name: Optional[str] = None
+    latitude: Optional[float] = None
+    longitude: Optional[float] = None
+    altitude: Optional[float] = None
+
+
+class VertiportFilterSchema(Schema):
+    id: Optional[UUID] = None
+    vertiport_code: Optional[str] = None
+    vertiport_name: Optional[str] = None
